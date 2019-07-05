e9803ab5891db1a41a5bf99ea73b92fad43953a8
Agarwal <ritika.agarwal@appliedis.com>
2019-07-05 18:02:39 +0530

    sdf

---
+++ APIAppTest/Controllers/CalculatorController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace APIAppTest.Controllers
+{
+    public class CalculatorController : ApiController
+    {
+        // GET api/values/5  
+        [HttpGet]
+        [System.Web.Http.Route("Api/Add")]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [HttpGet]
+        [System.Web.Http.Route("Api/Sub")]
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        
+        [HttpGet]
+        [System.Web.Http.Route("Api/Mul")]
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        [HttpGet]
+        [System.Web.Http.Route("Api/Div")]
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
